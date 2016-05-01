87e73569c8886196ac3d91b9ce6ab236f6ef5c26
matteoaccetturo <matteoaccetturo@yahoo.it>
2016-05-01 23:32:01 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+         <form action="" method="post">
+           
+            <div>
+                <input type="hidden" name="action" value="logout">
+                <input type="hidden" name="goto" value="/SEUdb/">
+                <input type="submit" value="Log out">
+            </div>
+    </form>
+    </body>
+</html>
